f3f7a855b6cc13f0df1e0d65147856b7306fb762
Mychal Thompson <mychalrthompson@gmail.com>
2023-09-26 20:15:04 -0400

    - Added `AppAboutInfoModal` for showing 3rd party credits/licenses. - Fixed issue where sync icon would stop spinning.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from "@angular/
 import { AsyncState, ComponentState } from "@lithiumjs/angular";
 import { Select } from "@ngxs/store";
 import { Observable } from "rxjs";
-import { distinctUntilChanged } from "rxjs/operators";
+import { distinctUntilChanged, filter } from "rxjs/operators";
 import { BaseComponent } from "./core/base-component";
 import { AppState } from "./state";
 import { OverlayHelpers, OverlayHelpersRef } from "./services/overlay-helpers";
 import { AppModSyncIndicatorModal } from "./modals/mod-sync-indicator";
+import { AppAboutInfoModal, DEPS_INFO_TOKEN } from "./modals/app-about-info";
 import { AppTheme } from "./models/app-theme";
+import { AppMessageHandler } from "./services/app-message-handler";
+import { AppMessageData } from "./models/app-message";
 
 @Component({
     selector: "app-root",
@@ -29,18 +32,25 @@ export class AppComponent extends BaseComponent {
 
     constructor (
         cdRef: ChangeDetectorRef,
-        overlay: OverlayHelpers
+        messageHandler: AppMessageHandler,
+        private readonly overlayHelpers: OverlayHelpers
     ) {
         super({ cdRef });
 
         let deployInProgressOverlayRef: OverlayHelpersRef | undefined;
+
+        messageHandler.messages$.pipe(
+            filter(message => message.id === "app:showAboutInfo")
+        ).subscribe(({ data }) => this.showAppAboutInfo(
+            data as AppMessageData<"app:showAboutInfo"> // TODO
+        ));
 
         // Show a loading indicator when app is syncing mod files to base deployment dir
         this.isDeployInProgress$.pipe(
             distinctUntilChanged()
         ).subscribe((deployInProgress) => {
             if (deployInProgress && !deployInProgressOverlayRef) {
-                deployInProgressOverlayRef = overlay.createFullScreen(AppModSyncIndicatorModal, {
+                deployInProgressOverlayRef = overlayHelpers.createFullScreen(AppModSyncIndicatorModal, {
                     width: "auto",
                     height: "auto",
                     minHeight: "10%",
@@ -55,5 +65,17 @@ export class AppComponent extends BaseComponent {
                 deployInProgressOverlayRef.close();
             }
         });
+    }
+
+    private showAppAboutInfo(aboutData: AppMessageData<"app:showAboutInfo">): void {
+        this.overlayHelpers.createFullScreen(AppAboutInfoModal, {
+            width: "50vw",
+            height: "auto",
+            maxHeight: "75vh",
+            center: true,
+            hasBackdrop: true,
+            disposeOnBackdropClick: true,
+            panelClass: "mat-app-background"
+        }, [[DEPS_INFO_TOKEN, aboutData]]);
     }
 }
