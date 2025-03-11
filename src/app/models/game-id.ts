f4fd236284c67966e71c652310bf9df002b58761
Mychal Thompson <mychalrthompson@gmail.com>
2025-03-11 11:35:05 -0400

    - Added default installation path for Oblivion DVD version.

--- src/app/models/game-id.ts
+++ src/app/models/game-id.ts
@@ -1,6 +1,7 @@
 export type GameId = 
       "$none"
     | "$unknown"
+    | "elder_scrolls_oblivion"
     | "fallout_4"
     | "fallout_new_vegas"
     | "starfield";
@@ -9,6 +10,7 @@ export namespace GameId {
 
     export const NONE: GameId = "$none";
     export const UNKNOWN: GameId = "$unknown";
+    export const ELDER_SCROLLS_OBLIVION: GameId = "elder_scrolls_oblivion";
     export const FALLOUT_4: GameId = "fallout_4";
     export const FALLOUT_NEW_VEGAS: GameId = "fallout_new_vegas";
     export const STARFIELD: GameId = "starfield";
